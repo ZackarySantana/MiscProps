715fe43ed09cb8668fd5a6fd6e6e96584b52b95a
Zackary Santana <zackzackyack@gmail.com>
2021-01-01 17:25:55 -0500

    Added better test cases and imported all test cases

--- src/fetchdata/test.js
+++ src/fetchdata/test.js
@@ -2,23 +2,50 @@ import React from "react";
 
 import Data from "./index";
 
+// This is just a dummy component that can be replaced with any component
+// The props is the data from the call
 const Display = (data) => {
 	console.log(data.title);
 	return <h1>Hey!</h1>;
 };
 
+// This implementation is showing off the Display component (which recieves the data from the API call as the props) being in the props of Data
+// This is useful for one liners that just want to pass the data to another nested component
 export const WithProps = () => {
 	return <Data Display={Display} whereToFetch="Bloop" />;
 };
 
+// This implementation is using the children of data and functions the same one as the top
+// This is useful when you want to pass other props to the display component
 export const WithChildren = () => {
 	return (
 		<Data whereToFetch="Bloop">
-			<Display />
+			<Display dummyProp={"this is a dummy prop!"} />
 		</Data>
 	);
 };
 
+// This implementation simply creates a <p> tag for every key:value recieved from the call
+// This is extremely useful when you just want to view the data this prop is getting without making a dummy component yourself
 export const WithDefaultComponent = () => {
 	return <Data whereToFetch="Bloop" />;
+};
+
+// This is an example of a custom fallback while the API is doing its request
+// You can pass any component inside, even a minature game!
+export const WithFallback = () => {
+	return (
+		<Data
+			Display={Display}
+			Fallback={<h1>Custom Fallback!</h1>}
+			whereToFetch="Bloop"
+		/>
+	);
+};
+
+// This is an example that retrieve's the data but then removes it from the cache 100ms later
+// This implementation removes it slightly later because it still needs to retrieve it for the first render
+// But after that, another rerender will re-fetch the data
+export const WithNoCache = () => {
+	return <Data Display={Display} cacheData={false} whereToFetch="Bloop" />;
 };
--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-// Components testing in main
-import TestFetchData from "./fetchdata/test";
+// COMPONENTS
+// Fetchdata library: To use make sure to correct the fetchD
+import {
+	WithProps,
+	WithChildren,
+	WithDefaultComponent,
+	WithFallback,
+	WithNoCache,
+} from "./fetchdata/test";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<TestFetchData />
+		<WithDefaultComponent />
 	</React.StrictMode>,
 	document.getElementById("root")
 );
