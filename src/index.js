da3b03044d716707bb188286fa1bb215faf746cc
Zackary Santana <zackzackyack@gmail.com>
2021-01-01 15:59:54 -0500

    Added basic skeleton shell

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import Data from "./fetchdata";
+
+import "./index.css";
+
+ReactDOM.render(
+	<React.StrictMode>
+		<Data />
+	</React.StrictMode>,
+	document.getElementById("root")
+);
