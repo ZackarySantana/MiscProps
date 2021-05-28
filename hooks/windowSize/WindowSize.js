9462ce7a3643886eef3eb4b612271351ccdf4d35
Zackary Santana <zackzackyack@gmail.com>
2021-05-28 15:54:42 -0400

    Added windowsize example and windowsize hook

--- hooks/windowSize/WindowSize.js
+++ hooks/windowSize/WindowSize.js
@@ -1,49 +1,72 @@
-import { useLayoutEffect, useCallback, useState, useRef, useMemo } from "react";
+import { useLayoutEffect, useState, useRef } from "react";
 
+// What it does:
+// - Checks if the size of the browser has changed
+// - When it has, rerender the component
+// - Using the arguments increase render performance and avoid useless rerenders
+// What are the arguments:
+// - updateOnWidths = An (optional) array to only update the component at specific widths
+// - updateOnHeights = An (optional) array to only update the component at specific heights
 export default function useWindowSize(updateOnWidths, updateOnHeights) {
   const [size, setSize] = useState([window.innerWidth, window.innerHeight]);
-  const oldSize = useRef(size);
-  const alwaysUpdate = useMemo(
-    () => !Array.isArray(updateOnWidths) && !Array.isArray(updateOnHeights),
-    [updateOnWidths, updateOnHeights]
-  );
-  const updateSize = useCallback(() => {
-    let update = alwaysUpdate;
-    if (!alwaysUpdate) {
-      if (Array.isArray(updateOnWidths) && updateOnWidths.length > 0) {
-        updateOnWidths.forEach((width) => {
-          if (oldSize.current[0] < width && window.innerWidth >= width) {
-            update = true;
-          } else if (oldSize.current[0] >= width && window.innerWidth < width) {
-            update = true;
-          }
-        });
-      }
-      if (Array.isArray(updateOnHeights)) {
-        updateOnHeights.forEach((height) => {
-          if (oldSize.current[1] < height && window.innerHeight >= height) {
-            update = true;
-          } else if (
-            oldSize.current[1] >= height &&
-            window.innerHeight < height
-          ) {
-            update = true;
-          }
-        });
-      }
-    }
-    if (update) {
-      setSize(() => {
-        oldSize.current = [window.innerWidth, window.innerHeight];
-        return oldSize.current;
-      });
-    }
-  }, [updateOnWidths, updateOnHeights, alwaysUpdate]);
+  // useState is used because we would like to rerender the component when the size changes
+
+  const oldSize = useRef(size); // Holds the old size of the element
+  // useRef is used to persist the old data while not updating the component
+
   useLayoutEffect(() => {
+    // Defines our function to test if we should update the size
+    let updateSize = () => {
+      // Tests if there is arrays, if not, this is true (so an update happens)
+      let update =
+        !Array.isArray(updateOnWidths) && !Array.isArray(updateOnHeights);
+      if (!update) {
+        // Tests if it should update based on the widths
+        if (Array.isArray(updateOnWidths) && updateOnWidths.length > 0) {
+          updateOnWidths.forEach((width) => {
+            if (oldSize.current[0] < width && window.innerWidth >= width) {
+              update = true;
+            } else if (
+              oldSize.current[0] >= width &&
+              window.innerWidth < width
+            ) {
+              update = true;
+            }
+          });
+        }
+
+        // Tests if it should update based on the heights
+        if (!update && Array.isArray(updateOnHeights)) {
+          updateOnHeights.forEach((height) => {
+            if (oldSize.current[1] < height && window.innerHeight >= height) {
+              update = true;
+            } else if (
+              oldSize.current[1] >= height &&
+              window.innerHeight < height
+            ) {
+              update = true;
+            }
+          });
+        }
+      }
+
+      // Should it update?
+      if (update) {
+        // Then change size and oldsize
+        setSize(() => {
+          oldSize.current = [window.innerWidth, window.innerHeight];
+          return oldSize.current;
+        });
+      }
+    };
+
+    // Registers the event listener
     window.addEventListener("resize", updateSize);
+
+    // Deregisters the event listener
     return () => {
       window.removeEventListener("resize", updateSize);
     };
-  }, [updateSize]);
+  }, [updateOnWidths, updateOnHeights]);
   return size;
 }
---
+++ hooks/windowSize/example.js
@@ -0,0 +1,34 @@
+import React from "react";
+import useWindowSize from "./WindowSize";
+
+// What it does:
+// - A component that displays the width and height of the window
+// - Every window resize, the component gets rerendered
+// Note:
+// - For performance, this isn't that bad since it won't be constantly changing but
+//   for big components, you should use the example below.
+export function DisplayDimensions() {
+  const [width, height] = useWindowSize(); // Calls hook, which returns array of [width, height]
+
+  return (
+    <div>
+      <p>Width of window: {width}</p>
+      <p>Height of window: {height}</p>
+    </div>
+  );
+}
+
+// What it does:
+// - A component that displays the width and height of the window
+// - It ONLY rerenders when the width changes past 800, or the height past 20 or 30
+// - This has increased performance but requires manual numbers
+export default function UpdateOnSize() {
+  const [width, height] = useWindowSize([800], [20, 30]); // Calls hook, which returns array of [width, height]
+
+  return (
+    <div>
+      <p>Width of window: {width}</p>
+      <p>Height of window: {height}</p>
+    </div>
+  );
+}
