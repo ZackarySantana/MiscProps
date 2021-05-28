206871e82bfdc0495d2b443abb464a4151be848c
Zackary Santana <zackzackyack@gmail.com>
2021-05-28 15:47:03 -0400

    Created example, added comments, and removed useless code in the hook

--- hooks/elementSize/ElementSize.js
+++ hooks/elementSize/ElementSize.js
@@ -1,53 +1,89 @@
-import { useLayoutEffect, useCallback, useState, useRef, useMemo } from "react";
+import { useLayoutEffect, useState, useRef } from "react";
 
+// What it does:
+// - Checks for the size changing when the browser is resized
+// - The size must change for the component to rerender
+// What are the arguments:
+// - ref = A (required) reference to the object that we want to use the size of (React's ref object)
+// - updateOnWidths = An (optional) array to only update the component at specific widths
+// - updateOnHeights = An (optional) array to only update the components at specific heights
 export default function useElementSize(ref, updateOnWidths, updateOnHeights) {
-  const [size, setSize] = useState([0, 0]);
-  const oldSize = useRef(size);
-  const alwaysUpdate = useMemo(
-    () => !Array.isArray(updateOnWidths) && !Array.isArray(updateOnHeights),
-    [updateOnWidths, updateOnHeights]
-  );
-  const updateSize = useCallback(() => {
-    let bounds = ref.current.getBoundingClientRect();
-    if (
-      oldSize.current[0] !== bounds.width ||
-      oldSize.current[1] !== bounds.height
-    ) {
-      let update = alwaysUpdate;
-      if (!alwaysUpdate) {
-        if (Array.isArray(updateOnWidths) && updateOnWidths.length > 0) {
-          updateOnWidths.forEach((width) => {
-            if (oldSize.current[0] < width && bounds.width >= width) {
-              update = true;
-            } else if (oldSize.current[0] >= width && bounds.width < width) {
-              update = true;
-            }
-          });
-        }
-        if (Array.isArray(updateOnHeights)) {
-          updateOnHeights.forEach((height) => {
-            if (oldSize.current[1] < height && bounds.height >= height) {
-              update = true;
-            } else if (oldSize.current[1] >= height && bounds.height < height) {
-              update = true;
-            }
-          });
-        }
-      }
-      if (update) {
-        setSize(() => {
-          oldSize.current = [bounds.width, bounds.height];
-          return oldSize.current;
-        });
-      }
-    }
-  }, [ref, updateOnWidths, updateOnHeights, alwaysUpdate]);
+  const [size, setSize] = useState([-1, -1]); // Used to keep track of the size of the element
+  // useState is used because we would like to rerender the component when the size changes
+
+  const oldSize = useRef(size); // Holds the old size of the element
+  // useRef is used to persist the old data while not updating the component
+
   useLayoutEffect(() => {
+    // Defines our function to test if we should update the size
+    const updateSize = () => {
+      // Gets the bounds of our element
+      let bounds = ref.current.getBoundingClientRect();
+      // Tests if the element changed in size
+      if (
+        oldSize.current[0] !== bounds.width ||
+        oldSize.current[1] !== bounds.height
+      ) {
+        // Tests if there is arrays, if not, this is true (so an update happens)
+        let update =
+          !Array.isArray(updateOnWidths) && !Array.isArray(updateOnHeights);
+        if (!update) {
+          // Tests if it is first render
+          if (oldSize.current[0] === -1) {
+            update = true;
+          }
+
+          // Tests if it should update based on the widths
+          if (
+            !update &&
+            Array.isArray(updateOnWidths) &&
+            updateOnWidths.length > 0
+          ) {
+            updateOnWidths.forEach((width) => {
+              if (oldSize.current[0] < width && bounds.width >= width) {
+                update = true;
+              } else if (oldSize.current[0] >= width && bounds.width < width) {
+                update = true;
+              }
+            });
+          }
+
+          // Tests if it should update based on the heights
+          if (!update && Array.isArray(updateOnHeights)) {
+            updateOnHeights.forEach((height) => {
+              if (oldSize.current[1] < height && bounds.height >= height) {
+                update = true;
+              } else if (
+                oldSize.current[1] >= height &&
+                bounds.height < height
+              ) {
+                update = true;
+              }
+            });
+          }
+        }
+
+        // Should it update?
+        if (update) {
+          // Then change size and oldsize
+          setSize(() => {
+            oldSize.current = [bounds.width, bounds.height];
+            return oldSize.current;
+          });
+        }
+      }
+    };
+
+    // Registers the event listener
     window.addEventListener("resize", updateSize);
+
+    // Runs the initial update size to grab the size
     updateSize();
+
+    // Deregisters the event listener
     return () => {
       window.removeEventListener("resize", updateSize);
     };
-  }, [updateSize]);
+  }, [ref, updateOnWidths, updateOnHeights]);
   return size;
 }
---
+++ hooks/elementSize/example.js
@@ -0,0 +1,39 @@
+import React, { useRef } from "react";
+import useElementSize from "./ElementSize";
+
+// What it does:
+// - A component that displays the width and height of an <h1>
+// - Every window resize, the dimensions of the <h1> get tested, if they
+//   change, then the component gets rerendered
+// Note:
+// - For performance, this isn't that bad since it won't be constantly changing but
+//   for big components, you should use the example below.
+export function DisplayDimensions() {
+  const ref = useRef(); // Creates the ref to use on the <h1>
+  const [width, height] = useElementSize(ref); // Calls hook, which returns array of [width, height]
+
+  return (
+    <div>
+      <h1 ref={ref}>Header</h1>
+      <p>Width of header: {width}</p>
+      <p>Height of header: {height}</p>
+    </div>
+  );
+}
+
+// What it does:
+// - A component that displays the width and height of an <h1>
+// - It ONLY rerenders when the width changes past 800, or the height past 20 or 30
+// - This has increased performance but requires manual numbers
+export default function UpdateOnSize() {
+  const ref = useRef(); // Creates the ref for the <h1>
+  const [width, height] = useElementSize(ref, [800], [20, 30]); // Calls hook, which returns array of [width, height]
+
+  return (
+    <div>
+      <h1 ref={ref}>Header</h1>
+      <p>Width of header: {width}</p>
+      <p>Height of header: {height}</p>
+    </div>
+  );
+}
